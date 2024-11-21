6020d5a25e14245c914db4b7a60502519394198d
sohailshams <sohailshamas@yahoo.com>
2024-11-21 22:38:42 +0000

    Made Food required property in FoodImage class.

--- HomeCook.Api/Models/FoodImage.cs
+++ HomeCook.Api/Models/FoodImage.cs
@@ -4,7 +4,7 @@
     {
         public Guid Id { get; set; }
         public Guid FoodId { get; set; }  
-        public Food Food { get; set; }
+        public required Food Food { get; set; }
         public required string Image { get; set; }
     }
 }
