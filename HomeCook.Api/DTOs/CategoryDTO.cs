a0f02fa1d4f9b94b7714d501d02a35cafd58c035
sohailshams <sohailshamas@yahoo.com>
2024-11-22 22:16:20 +0000

    Created CategoryDTO.

---
+++ HomeCook.Api/DTOs/CategoryDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeCook.Api.DTOs
+{
+    public class CategoryDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
