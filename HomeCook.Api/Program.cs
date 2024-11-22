c7ae9275bbb2179a98bd54acf5c637e584029415
sohailshams <sohailshamas@yahoo.com>
2024-11-22 22:20:11 +0000

    Added dependency injection for ICategoriesReposity and ICategoriesService.

--- HomeCook.Api/Program.cs
+++ HomeCook.Api/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using HomeCook.Api.EntityFramework;
+using HomeCook.Api.EntityFramework.Repositories;
+using HomeCook.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,9 @@ builder.Services.AddSwaggerGen();
 var connectionString = builder.Configuration.GetConnectionString("DbConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped <ICategoriesReposity, CategoriesRepository>();
+builder.Services.AddScoped<ICategoriesService, CategoriesService>();
 
 var app = builder.Build();
 
