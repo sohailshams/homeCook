0232b2ae1bcace3e8cc2d63974caf7d1345b614a
sohailshams <sohailshamas@yahoo.com>
2024-11-23 22:35:14 +0000

    Updated code to use AutoMapper to map  Category model to CategoryDTO.

--- HomeCook.Api/Services/CategoriesService.cs
+++ HomeCook.Api/Services/CategoriesService.cs
@@ -1,28 +1,23 @@
-﻿using HomeCook.Api.DTOs;
+﻿using AutoMapper;
+using HomeCook.Api.DTOs;
 using HomeCook.Api.EntityFramework.Repositories;
-using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace HomeCook.Api.Services
 {
     public class CategoriesService : ICategoriesService
     {
         private readonly ICategoriesReposity _categoriesReposity;
-        public CategoriesService(ICategoriesReposity categoriesReposity)
+        private readonly IMapper _mapper;
+
+        public CategoriesService(ICategoriesReposity categoriesReposity, IMapper mapper)
         {
             _categoriesReposity = categoriesReposity;
+            _mapper = mapper;
         }
         public async Task<List<CategoryDTO>> GetCategoriesAsync()
         {
             var categories = await _categoriesReposity.GetCategoriesAsync();
-            var categoriesDto = new List<CategoryDTO>();
-            foreach (var category in categories)
-            {
-                categoriesDto.Add(new CategoryDTO()
-                {
-                    Id = category.Id,
-                    Name = category.Name,
-                });
-            }
+            var categoriesDto = _mapper.Map<List<CategoryDTO>>(categories);
             return categoriesDto;
         }
     }
