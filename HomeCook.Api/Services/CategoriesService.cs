d079f5dcedf1fd6eff04b6c26fb7dc5ae9900bd2
sohailshams <sohailshamas@yahoo.com>
2024-11-24 17:57:45 +0000

    Added comments.

--- HomeCook.Api/Services/CategoriesService.cs
+++ HomeCook.Api/Services/CategoriesService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using HomeCook.Api.DTOs;
 using HomeCook.Api.EntityFramework.Repositories;
+using HomeCook.Api.Models;
 
 namespace HomeCook.Api.Services
 {
@@ -16,7 +17,10 @@ namespace HomeCook.Api.Services
         }
         public async Task<List<CategoryDTO>> GetCategoriesAsync()
         {
+            // get categories from db
             var categories = await _categoriesReposity.GetCategoriesAsync();
+
+            // map categories<Category> to categories<CategoryDTO>
             var categoriesDto = _mapper.Map<List<CategoryDTO>>(categories);
             return categoriesDto;
         }
