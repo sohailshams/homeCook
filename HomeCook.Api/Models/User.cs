400ea36eaaca75a1c8e8b4dc7dcd7f2f3b8436bd
sohailshams <sohailshamas@yahoo.com>
2024-12-02 23:38:34 +0000

    Installed identity, extended Uder from IdentityUser and initialized DateTime property.

--- HomeCook.Api/Models/User.cs
+++ HomeCook.Api/Models/User.cs
@@ -1,15 +1,16 @@
 ﻿using HomeCook.Api.Enums;
+using Microsoft.AspNetCore.Identity;
 
 namespace HomeCook.Api.Models
 {
-    public class User
+    public class User : IdentityUser
     {
         public Guid Id { get; set; }
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
         public required string Email { get; set; }
-        public DateTime CreatedAt { get; set; }
-         public UserRole Role { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public UserRole Role { get; set; }
         public Profile? Profile { get; set; }
 
     }
