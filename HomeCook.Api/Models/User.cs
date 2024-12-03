44fb55c5537d4c3040565e91f10483f705f9e2c0
sohailshams <sohailshamas@yahoo.com>
2024-12-03 23:05:21 +0000

    extended User from IdentityUser.

--- HomeCook.Api/Models/User.cs
+++ HomeCook.Api/Models/User.cs
@@ -5,13 +5,19 @@ namespace HomeCook.Api.Models
 {
     public class User : IdentityUser
     {
-        public Guid Id { get; set; }
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
-        public required string Email { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        //public Guid Id { get; set; }
+        //public string FirstName { get; set; }
+        //public string LastName { get; set; }
+        //public string Email { get; set; }
+        //public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public UserRole Role { get; set; }
         public Profile? Profile { get; set; }
 
+        //public User()
+        //{
+        //    FirstName = string.Empty;
+        //    LastName = string.Empty;
+        //    Email = string.Empty;
+        //}
     }
 }
