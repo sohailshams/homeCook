e06bc83af7b8468dc80717d40029edd79603ddc9
sohailshams <sohailshamas@yahoo.com>
2024-12-15 22:11:06 +0000

    Added FoodController.

---
+++ HomeCook.Api/Controllers/FoodController.cs
@@ -0,0 +1,26 @@
+﻿using HomeCook.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeCook.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        private readonly ILogger<FoodController> _logger;
+        private readonly IFoodService _foodService;
+
+        public FoodController(ILogger<FoodController> logger, IFoodService foodService)
+        {
+            _logger = logger;
+            _foodService = foodService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetFoodList()
+        {
+            var food = await _foodService.GetFoodListAsync();
+            return Ok(food);
+        }
+    }
+}
