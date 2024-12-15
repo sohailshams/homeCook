f2863adbad9d5ee0d5642edf4d449891df405249
sohailshams <sohailshamas@yahoo.com>
2024-12-15 22:11:52 +0000

    Added Food service and related interface.

---
+++ HomeCook.Api/Services/FoodService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using HomeCook.Api.DTOs;
+using HomeCook.Api.EntityFramework.Repositories;
+
+namespace HomeCook.Api.Services
+{
+    public class FoodService : IFoodService
+    {
+        private readonly IFoodRepository _foodRepository;
+        private readonly IMapper _mapper;
+        public FoodService(IFoodRepository foodRepository, IMapper mapper)
+        {
+            _foodRepository = foodRepository;
+            _mapper = mapper;
+
+        }
+
+        public async Task<List<FoodDTO>> GetFoodListAsync()
+        {
+            // get food list from db
+            var food = await _foodRepository.GetFoodListAsync();
+
+            // map food list<Food> to food list<FoodDTO>
+            var foodDto = _mapper.Map<List<FoodDTO>>(food);
+            return foodDto;
+        }
+    }
+}
---
+++ HomeCook.Api/Services/IFoodService.cs
@@ -0,0 +1,9 @@
+﻿using HomeCook.Api.DTOs;
+
+namespace HomeCook.Api.Services
+{
+    public interface IFoodService
+    {
+        public Task<List<FoodDTO>> GetFoodListAsync();
+    }
+}
