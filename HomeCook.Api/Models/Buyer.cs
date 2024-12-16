9fe605627d5a7d393d34a7be2d63ce459d03139b
sohailshams <sohailshamas@yahoo.com>
2024-12-16 23:12:25 +0000

    Removed Seller and Buyer models.

--- HomeCook.Api/Models/Buyer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HomeCook.Api.Models
-{
-    public class Buyer : User
-    {
-        public List<Food> Foods { get; set; } = new List<Food>();
-
-    }
-}
--- HomeCook.Api/Models/Seller.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace HomeCook.Api.Models
-{
-    public class Seller : User
-    {
-    public List<Food> Foods { get; set; } = new List<Food>();
-    }
-}
