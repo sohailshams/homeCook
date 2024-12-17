0e34d706aafb78dd8cae8eb9cd712d7641909081
sohailshams <sohailshamas@yahoo.com>
2024-12-17 17:01:31 +0000

    Created project migrations.

--- HomeCook.Api/Migrations/20241216224744_InitialMigrations.Designer.cs
+++ HomeCook.Api/Migrations/20241216224744_InitialMigrations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HomeCook.Api.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HomeCook.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241216224744_InitialMigrations")]
+    partial class InitialMigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -53,7 +56,7 @@ namespace HomeCook.Api.Migrations
                         .IsRequired()
                         .HasColumnType("text");
 
-                    b.Property<Guid?>("FoodImageId")
+                    b.Property<Guid>("FoodImageId")
                         .HasColumnType("uuid");
 
                     b.Property<string>("Ingredients")
