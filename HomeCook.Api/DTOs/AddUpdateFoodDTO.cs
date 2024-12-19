8fd25fcfdeee63d520af5467f60921ed4d3ec68a
sohailshams <sohailshamas@yahoo.com>
2024-12-19 17:17:00 +0000

    Updated AddFoodDTO to AddUpdateFoodDTO.

---
+++ HomeCook.Api/DTOs/AddUpdateFoodDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace HomeCook.Api.DTOs
+{
+    public class AddUpdateFoodDTO
+    {
+        public string Name { get; set; } 
+        public string Description { get; set; } 
+        public string Ingredients { get; set; } 
+        public decimal Price { get; set; } 
+        public int QuantityAvailable { get; set; } 
+        public DateTime AvailableDate { get; set; } 
+        public Guid CategoryId { get; set; }
+        public List<string>? FoodImageUrls { get; set; } 
+        public string SellerId { get; set; }
+
+    }
+}
