8e5f2eb5a21de1ed6d0746dbd6fc9d81efab2c4d
sohailshams <sohailshamas@yahoo.com>
2024-12-31 23:27:07 +0000

    Added UserService logic to get a user info by id and user interface.

---
+++ HomeCook.Api/Services/UserService.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using HomeCook.Api.DTOs;
+using HomeCook.Api.EntityFramework.Repositories;
+using HomeCook.Api.Exceptions;
+using HomeCook.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace HomeCook.Api.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IMapper _mapper;
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IHttpContextAccessor httpContextAccessor, IMapper mapper, IUserRepository userRepository )
+        {
+            _mapper = mapper;
+            _userRepository = userRepository;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public async Task<UserInfoDTO?> GetUserInfoAsync()
+        {
+            try
+            {
+                var loggedInUserId = (_httpContextAccessor.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value) ?? throw new UnauthorizedAccessException("Unauthorized user.");
+
+                var user = await _userRepository.GetUserByIdAsync(loggedInUserId) ?? throw new NotFoundException("User not found.");
+
+                //Map user model to UserInfoDTO
+                var userInfoDto = _mapper.Map<UserInfoDTO>(user);
+
+                return userInfoDto;
+
+            }
+            catch (DbUpdateException exception)
+            {
+                throw new DatabaseOperationException("Failed to get a user.", exception);
+            }
+        }
+    }
+}
