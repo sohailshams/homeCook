786a1cadf10964a96402bdec1a8111cd23f30f54
sohailshams <sohailshamas@yahoo.com>
2024-12-31 23:25:23 +0000

    Added UserInfoDTO.

---
+++ HomeCook.Api/DTOs/UserInfoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeCook.Api.DTOs
+{
+    public class UserInfoDTO
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public bool IsProfileComplete { get; set; }
+    }
+}
