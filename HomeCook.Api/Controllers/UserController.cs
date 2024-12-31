fd1caf86ee3772d11dca0d2665091c807c2972e7
sohailshams <sohailshamas@yahoo.com>
2024-12-31 23:25:06 +0000

    Added UserController to get user info.

---
+++ HomeCook.Api/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using HomeCook.Api.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeCook.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IUserService _userService;
+        public UserController(ILogger<UserController> logger, IUserService userService)
+        {
+            _logger = logger;
+            _userService = userService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetUser()
+        {
+            var user = await _userService.GetUserInfoAsync();
+            return Ok(user);
+        }
+    }
+}
