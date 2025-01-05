1f125091259d3e627c9e7c88c2779000d6435a50
sohailshams <sohailshamas@yahoo.com>
2025-01-05 21:52:25 +0000

    Added LogoutController controller.

---
+++ HomeCook.Api/Controllers/LogoutController.cs
@@ -0,0 +1,32 @@
+﻿using HomeCook.Api.DTOs;
+using HomeCook.Api.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeCook.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogoutController : ControllerBase
+    {
+        private readonly SignInManager<User> _signInManager;
+
+        public LogoutController(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Ok(new { message = "Logout successful" });
+
+        }
+    }
+}
