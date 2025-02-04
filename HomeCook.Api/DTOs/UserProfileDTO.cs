c8f46a99f4c322e44b9e9e210c44b551f4e38549
sohailshams <sohailshamas@yahoo.com>
2025-02-04 23:13:56 +0000

    Created UserProfileDTO to get required data from db.

---
+++ HomeCook.Api/DTOs/UserProfileDTO.cs
@@ -0,0 +1,20 @@
+﻿using HomeCook.Api.Models;
+
+namespace HomeCook.Api.DTOs
+{
+    public class UserProfileDTO
+    {
+        public Guid Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Address { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string City { get; set; }
+        public required string Country { get; set; }
+        public required string PostCode { get; set; }
+        public string? Bio { get; set; }
+        public string? ProfileImage { get; set; }
+        public required string UserId { get; set; }
+        public required User User { get; set; }
+    }
+}
