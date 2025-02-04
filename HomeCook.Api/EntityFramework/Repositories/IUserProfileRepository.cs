5b337d58e99f6cfe95a3be04d2d08a7e31015d80
sohailshams <sohailshamas@yahoo.com>
2025-02-04 23:15:50 +0000

    Added repository logic to retrieve profile data from db.

---
+++ HomeCook.Api/EntityFramework/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,10 @@
+﻿using HomeCook.Api.DTOs;
+using HomeCook.Api.Models;
+
+namespace HomeCook.Api.EntityFramework.Repositories
+{
+    public interface IUserProfileRepository
+    {
+        Task<Profile?> GetUserProfileByIdAsync(string userId);
+    }
+}
---
+++ HomeCook.Api/EntityFramework/Repositories/UserProfileRepository.cs
@@ -0,0 +1,22 @@
+﻿using HomeCook.Api.DTOs;
+using HomeCook.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeCook.Api.EntityFramework.Repositories
+{
+    public class UserProfileRepository : IUserProfileRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public UserProfileRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<Profile?> GetUserProfileByIdAsync(string userId)
+        {
+            var userProfile = await _dbContext.Profiles.FirstOrDefaultAsync(p => p.UserId == userId);
+
+            return userProfile;
+        }
+    }
+}
