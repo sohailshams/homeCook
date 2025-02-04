cdca187da5fcc828464afa919fe70c3e53f0dc02
sohailshams <sohailshamas@yahoo.com>
2025-02-04 23:13:16 +0000

    Added profile controller end point.

---
+++ HomeCook.Api/Controllers/ProfileController.cs
@@ -0,0 +1,31 @@
+﻿using HomeCook.Api.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeCook.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private readonly ILogger<ProfileController> _logger;
+        private readonly IUserProfileService _userProfileService;
+        public ProfileController(ILogger<ProfileController> logger, IUserProfileService userProfileService)
+        {
+            _logger = logger;
+            _userProfileService = userProfileService;
+        }
+
+        [HttpGet]
+        //[Authorize]
+        [Route("{userId}")]
+        public async Task<IActionResult> GetUserProfile([FromRoute] string userId)
+        {
+            var userProfile = await _userProfileService.GetUserProfileAsync(userId);
+
+            return Ok(userProfile);
+        }
+
+    }
+}
