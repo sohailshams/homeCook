e0ee1a64aab94cefbdc9b3e94639964beb2d34d7
sohailshams <sohailshamas@yahoo.com>
2025-02-24 23:08:04 +0000

    Updated IUserProfileService interface with UpdateUserProfileAsync method.

--- HomeCook.Api/Services/IUserProfileService.cs
+++ HomeCook.Api/Services/IUserProfileService.cs
@@ -6,5 +6,6 @@ namespace HomeCook.Api.Services
     {
         public Task<UserProfileDTO?> GetUserProfileAsync(string userId);
         public Task<AddUpdateProfileDTO> AddUserProfileAsync(AddUpdateProfileDTO addUpdateProfileDTO);
+        public Task<UserProfileDTO?> UpdateUserProfileAsync(AddUpdateProfileDTO updateProfile);
     }
 }
