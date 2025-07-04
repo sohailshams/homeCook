65f8dec8117aef382d4833698042b60829156b81
sohailshams <sohailshamas@yahoo.com>
2025-07-04 22:44:37 +0100

    Created constants.

---
+++ HomeCook.Api/Constants/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeCook.Api.Constants
+{
+    public static class Constants
+    {
+        public const string EmailTo = "sohailshamsramay@gmail.com";
+        public const string EmailFrom = "sohailshamsramay@gmail.com";
+        public const string EmailSubject = "Food Quantity Updated Failed";
+        public const string PlainTextContent = "Failed to update the quantity of food item.";
+        public const string HtmlContentBody = "<strong>Failed to update the quantity of food item.</strong>";
+
+    }
+}
