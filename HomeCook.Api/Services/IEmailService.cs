1660587fad937ed52f67f29a017a65147ee31b26
sohailshams <sohailshamas@yahoo.com>
2025-07-04 22:45:29 +0100

    Added EmailService interface and its implementation.

---
+++ HomeCook.Api/Services/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace HomeCook.Api.Services
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmailAsync(string from_email, string to_email, string subject, string plainTextContent, string htmlContent);
+    }
+}
