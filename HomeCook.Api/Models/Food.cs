7fafbb57736a09da8de95121ae6644d29ac88f47
sohailshams <sohailshamas@yahoo.com>
2025-11-30 22:49:42 +0000

    Update model, add PostCode property.

--- HomeCook.Api/Models/Food.cs
+++ HomeCook.Api/Models/Food.cs
@@ -16,6 +16,7 @@ namespace HomeCook.Api.Models
         public List<FoodImage>? FoodImage { get; set; }
         public required Guid CategoryId { get; set; }
         public Category Category { get; set; } = null!;
+        public required string PostCode { get; set; }
 
         // Added for full-text search
         public NpgsqlTsVector SearchVector { get; set; } = null!;
