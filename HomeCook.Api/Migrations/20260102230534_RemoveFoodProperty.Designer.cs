afe7e97efbdcfcd8f3576fd32d7f09a6594a578c
sohailshams <sohailshamas@yahoo.com>
2026-01-03 23:39:08 +0000

    Replace FoodImage and Category with DTOs and update migrations.

--- HomeCook.Api/Migrations/20260102230534_RemoveFoodProperty.Designer.cs
+++ HomeCook.Api/Migrations/20260102230534_RemoveFoodProperty.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using HomeCook.Api.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using NpgsqlTypes;
@@ -13,9 +14,11 @@ using NpgsqlTypes;
 namespace HomeCook.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260102230534_RemoveFoodProperty")]
+    partial class RemoveFoodProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HomeCook.Api/Migrations/20260102230534_RemoveFoodProperty.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeCook.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveFoodProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
