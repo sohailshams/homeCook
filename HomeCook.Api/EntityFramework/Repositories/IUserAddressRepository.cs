ba731b0bf0d162134c5f332d73a5927960334fad
sohailshams <sohailshamas@yahoo.com>
2026-02-01 23:40:31 +0000

    Add UserAddressRepository interface and repository logic.

---
+++ HomeCook.Api/EntityFramework/Repositories/IUserAddressRepository.cs
@@ -0,0 +1,10 @@
+﻿using HomeCook.Api.Models;
+
+namespace HomeCook.Api.EntityFramework.Repositories
+{
+    public interface IUserAddressRepository
+    {
+        Task<List<Address>> GetUserAddressListByIdAsync(Guid userId);
+        Task<Address> AddUserAddressAsync(Address address);
+    }
+}
---
+++ HomeCook.Api/EntityFramework/Repositories/UserAddressRepository.cs
@@ -0,0 +1,29 @@
+﻿using HomeCook.Api.DTOs;
+using HomeCook.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeCook.Api.EntityFramework.Repositories
+{
+    public class UserAddressRepository : IUserAddressRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public UserAddressRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Address>> GetUserAddressListByIdAsync(Guid userId)
+        {
+            var userAddressList = await _dbContext.Addresses.Where(p => p.UserId == userId).ToListAsync();
+
+            return userAddressList;
+        }
+        public async Task<Address> AddUserAddressAsync(Address address)
+        {
+            await _dbContext.Addresses.AddAsync(address);
+            await _dbContext.SaveChangesAsync();
+            return address;
+        }
+    }
+}
