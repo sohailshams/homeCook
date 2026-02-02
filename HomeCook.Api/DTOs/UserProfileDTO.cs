77b66822414c019cae304c263590e670f93e2996
sohailshams <sohailshamas@yahoo.com>
2026-02-02 23:38:29 +0000

    Add properties IsPrimary and Line1.

--- HomeCook.Api/DTOs/UserProfileDTO.cs
+++ HomeCook.Api/DTOs/UserProfileDTO.cs
@@ -7,13 +7,14 @@ namespace HomeCook.Api.DTOs
         public Guid Id { get; set; }
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
-        public required string Address { get; set; }
+        public required string Line1 { get; set; }
         public required string PhoneNumber { get; set; }
         public required string City { get; set; }
         public required string Country { get; set; }
         public required string PostCode { get; set; }
         public string? Bio { get; set; }
         public string? ProfileImage { get; set; }
-        public required string UserId { get; set; }
+        public required Guid UserId { get; set; }
+        public bool IsPrimary { get; set; }
     }
 }
