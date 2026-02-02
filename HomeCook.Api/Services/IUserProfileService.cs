c272b426e04d326aa6a9f780187e7647e2d3be3d
sohailshams <sohailshamas@yahoo.com>
2026-02-02 23:39:14 +0000

    Update return type dto to UserProfileDTO.

--- HomeCook.Api/Services/IUserProfileService.cs
+++ HomeCook.Api/Services/IUserProfileService.cs
@@ -5,7 +5,7 @@ namespace HomeCook.Api.Services
     public interface IUserProfileService
     {
         public Task<UserProfileDTO?> GetUserProfileAsync(Guid userId);
-        public Task<AddUpdateProfileDTO> AddUserProfileAsync(AddUpdateProfileDTO addUpdateProfileDTO);
+        public Task<UserProfileDTO> AddUserProfileAsync(AddUpdateProfileDTO addUpdateProfileDTO);
         public Task<UserProfileDTO?> UpdateUserProfileAsync(AddUpdateProfileDTO updateProfile);
     }
 }
