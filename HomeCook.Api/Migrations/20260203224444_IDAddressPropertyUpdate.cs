d4e64d8c4fd5de2d38fb5f5ce48411e497d7814a
sohailshams <sohailshamas@yahoo.com>
2026-02-03 23:08:30 +0000

    Add new migrations.

---
+++ HomeCook.Api/Migrations/20260203224444_IDAddressPropertyUpdate.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeCook.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class IDAddressPropertyUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Profiles",
+                newName: "ProfileId");
+
+            migrationBuilder.RenameColumn(
+                name: "Line1",
+                table: "Addresses",
+                newName: "AddressLine1");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Addresses",
+                newName: "AddressId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProfileId",
+                table: "Profiles",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "AddressLine1",
+                table: "Addresses",
+                newName: "Line1");
+
+            migrationBuilder.RenameColumn(
+                name: "AddressId",
+                table: "Addresses",
+                newName: "Id");
+        }
+    }
+}
