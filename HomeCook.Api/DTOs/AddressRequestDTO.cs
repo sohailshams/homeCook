cfbe7471fa3b00fac4987fadace29ad072f0b633
sohailshams <sohailshamas@yahoo.com>
2026-02-21 20:01:37 +0000

    Add AddressRequestDTO.

---
+++ HomeCook.Api/DTOs/AddressRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using HomeCook.Api.Models;
+
+namespace HomeCook.Api.DTOs
+{
+    public class AddressRequestDTO
+    {
+        public required Guid UserId { get; set; }
+        public required string AddressLine1 { get; set; }
+        public required string City { get; set; }
+        public required string Country { get; set; }
+        public required string PostCode { get; set; }
+        public bool IsPrimary { get; set; }
+    }
+}
