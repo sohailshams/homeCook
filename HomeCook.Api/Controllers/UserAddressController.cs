e9b07d07b00760015728df980c6fdd257d101665
sohailshams <sohailshamas@yahoo.com>
2026-02-21 20:00:50 +0000

    Add UserAddressController for address update.

---
+++ HomeCook.Api/Controllers/UserAddressController.cs
@@ -0,0 +1,30 @@
+﻿using HomeCook.Api.DTOs;
+using HomeCook.Api.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeCook.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAddressController : ControllerBase
+    {
+        private readonly ILogger<UserAddressController> _logger;
+        private readonly IUserAddressService _userAddressService;
+        public UserAddressController(
+            ILogger<UserAddressController> logger,
+            IUserAddressService userAddressService)
+        {
+            _logger = logger;
+            _userAddressService = userAddressService;
+        }
+
+        [HttpPut("AddUserAddress")]
+        [Authorize]
+        public async Task<IActionResult> AddUserAddress([FromBody] AddressRequestDTO addAddressDTO)
+        {
+            var newAddress = await _userAddressService.AddAddressAsync(addAddressDTO);
+            return Ok(newAddress);
+        }
+    }
+}
