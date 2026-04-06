0f20f9ca9c7910b9810b2f7a514b901bb6648635
sohailshams <sohailshamas@yahoo.com>
2026-04-06 18:54:19 +0100

    Add PostcodeService and interface to Longitude and Latitude.

---
+++ HomeCook.Api/Services/IPostcodeService.cs
@@ -0,0 +1,9 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace HomeCook.Api.Services
+{
+    public interface IPostcodeService
+    {
+        Task<Point> GetLocationAsync(string postcode);
+    }
+}
---
+++ HomeCook.Api/Services/PostcodeService.cs
@@ -0,0 +1,33 @@
+﻿
+using HomeCook.Api.DTOs;
+using HomeCook.Api.Exceptions;
+using NetTopologySuite.Geometries;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace HomeCook.Api.Services
+{
+    public class PostcodeService : IPostcodeService
+    {
+        private readonly HttpClient _httpClient;
+
+        public PostcodeService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<Point> GetLocationAsync(string postcode)
+        {
+            var cleaned = postcode.Replace(" ", "");
+
+            var response = await _httpClient.GetFromJsonAsync<PostcodeResponseDto>($"postcodes/{cleaned}");
+
+            if (response?.Status != 200 || response.Result == null)
+                throw new NotFoundException($"Could not find coordinates for postcode {postcode}.");
+
+            return new Point(response.Result.Longitude, response.Result.Latitude)
+            {
+                SRID = 4326
+            };
+        }
+    }
+}
