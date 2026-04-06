7430946b4c8e7a6af4a5739b8fcc0761df9a555c
sohailshams <sohailshamas@yahoo.com>
2026-04-06 18:53:17 +0100

    Add PostcodeResponseDto.

---
+++ HomeCook.Api/DTOs/PostcodeResponseDto.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+using HomeCook.Api.Models;
+
+namespace HomeCook.Api.DTOs
+{
+    public class PostcodeResponseDto
+    {
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+
+        [JsonPropertyName("result")]
+        public PostcodeResult? Result { get; set; }
+    }
+
+    public class PostcodeResult
+    {
+        [JsonPropertyName("latitude")]
+        public double Latitude { get; set; }
+
+        [JsonPropertyName("longitude")]
+        public double Longitude { get; set; }
+    }
+}
